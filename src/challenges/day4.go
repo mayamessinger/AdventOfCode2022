9b97df10274eb59c2b059106e2d7bf8d6bc75d39
Maya Messinger <maya.messinger@gmail.com>
2022-12-04 12:02:00 -0600

    Day 4: part 1

---
+++ src/challenges/day4.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	// part 1
+	fmt.Println(containedRangeSum())
+}
+
+func containedRangeSum() int {
+	sum := 0
+
+	for _, line := range ReadFile("./resources/day4.txt") {
+		range1, range2 := parseRanges(line)
+		if isFullyContainedRange(range1, range2) {
+			sum += 1
+		}
+	}
+
+	return sum
+}
+
+func parseRanges(line string) ([]int, []int) {
+	rangeStrings := strings.Split(line, ",")
+	range1 := strings.Split(rangeStrings[0], "-")
+	range1Start, _ := strconv.Atoi(range1[0])
+	range1End, _ := strconv.Atoi(range1[1])
+	range2 := strings.Split(rangeStrings[1], "-")
+	range2Start, _ := strconv.Atoi(range2[0])
+	range2End, _ := strconv.Atoi(range2[1])
+
+	return []int{range1Start, range1End}, []int{range2Start, range2End}
+}
+
+func isFullyContainedRange(range1 []int, range2 []int) bool {
+	if (range1[0] >= range2[0] && range1[1] <= range2[1]) ||
+		(range2[0] >= range1[0] && range2[1] <= range1[1]) {
+		return true
+	}
+
+	return false
+}
