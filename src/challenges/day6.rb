e3a86ed3a13dda8a85fca2ac71fb86bcd61813cf
Maya Messinger <maya.messinger@gmail.com>
2022-12-06 20:23:04 -0600

    Day 6: part 2

--- src/challenges/day6.rb
+++ src/challenges/day6.rb
@@ -5,24 +5,26 @@ module Challenges
         include Helper
     
         def main
-            puts get_packet_marker_index
+            puts get_marker_index 4
+
+            puts get_marker_index 14
         end
 
-        def get_packet_marker_index
+        def get_marker_index(marker_length)
             stream_chars = read_file("./resources/day6.txt")
 
-            get_packet_start_of_stream(stream_chars[0].split(''))
+            get_marker_of_stream(stream_chars[0].split(''), marker_length)
         end
 
-        def get_packet_start_of_stream(stream_chars)
+        def get_marker_of_stream(stream_chars, marker_length)
             index = 0
             most_recent_chars = Array.new
 
             while index < stream_chars.length do
                 next_char = stream_chars[index]
-                adjust_recent_chars(most_recent_chars, stream_chars[index])
+                adjust_recent_chars(most_recent_chars, stream_chars[index], marker_length)
 
-                if is_packet_start(most_recent_chars)
+                if is_packet_start(most_recent_chars, marker_length)
                     return index + 1
                 end
 
@@ -30,16 +32,20 @@ module Challenges
             end
         end
 
-        def adjust_recent_chars(most_recent_chars, next_char)
-            if (most_recent_chars.length == 4)
+        def adjust_recent_chars(most_recent_chars, next_char, marker_length)
+            if (most_recent_chars.length == marker_length)
                 most_recent_chars.shift
             end
 
             most_recent_chars.push(next_char)
         end
 
-        def is_packet_start(most_recent_chars)
-            most_recent_chars.uniq.length == 4
+        def is_packet_start(most_recent_chars, marker_length)
+            if most_recent_chars.length != marker_length
+                return false
+            end
+
+            most_recent_chars.uniq.length == marker_length
         end
     end
 end
